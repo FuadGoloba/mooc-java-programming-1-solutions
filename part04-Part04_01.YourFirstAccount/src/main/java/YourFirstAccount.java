437db1d35b9b967d014162ed23fc6e4f00eba379
FuadGoloba <fuadgoloba@gmail.com>
2023-09-11 23:51:44 +0100

    Part04_01.YourFirstAccount

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -1,17 +1,12 @@
 
-
 public class YourFirstAccount {
 
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
-        Account FuadAccount = new Account("Fuad's account", 100.00);
-        
-        //System.out.println("Initial state");
-        //System.out.println(FuadAccount);
-        FuadAccount.deposit(20.00);
-        //System.out.println("The balance on Fuad's Account is now:" + FuadAccount.saldo());
-        System.out.println(FuadAccount.saldo());
-        
+        Account Fuad = new Account("Fuad Goloba", 100.0);
+        Fuad.deposit(20.0);
+        System.out.println(Fuad);
     }
 }
+
